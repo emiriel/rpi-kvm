c335ee3ae4b72e95d9cbb004bc8bbda5d2737d5e
Erwann Miriel <erwann@miriel.pro>
2025-10-19 00:10:31 +0200

    fix: add missing tmux config for touch pi hat

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,13 +12,14 @@ initRpiKvmTmux()
     tmux split-window -v -t rpi-kvm
 	tmux select-pane -t rpi-kvm:kvm.3
     tmux split-window -v -t rpi-kvm
+    tmux split-window -v -t rpi-kvm
 
     tmux send-keys -t rpi-kvm:kvm.0 'cd $RPI_KVM_PATH && reset && sudo ./rpi_kvm/kvm_service.py' C-m
     tmux send-keys -t rpi-kvm:kvm.1 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/web.py' C-m
     tmux send-keys -t rpi-kvm:kvm.2 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/info_hub.py' C-m
     tmux send-keys -t rpi-kvm:kvm.3 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/mouse.py' C-m
     tmux send-keys -t rpi-kvm:kvm.4 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/keyboard.py' C-m
-    tmux send-keys -t rpi-kvm:kvm.4 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/touch_phat.py' C-m
+    tmux send-keys -t rpi-kvm:kvm.5 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/touch_phat.py' C-m
 
 	tmux select-pane -t rpi-kvm:kvm.0
 }
